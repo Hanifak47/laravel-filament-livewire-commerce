b9b8a669bcd0fed99139447e0ae9545c4852e7ef
Hanifak47 <hanifsmurf@gmail.com>
2025-04-21 19:59:04 +0700

    +model migration dan relation

---
+++ database/migrations/2025_04_21_033150_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            // field category_id merujuk pada field id pada model categories (category yang digunakan adalah versi jamakknya)
+            // cascade on delete menandakan jika ada row pada categories yang dihapus maka seluruh record pada tabel ini yang merujuk pada row tersebut juga terhapus
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained('brands')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->json('images')->nullable();
+            $table->longText('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('in_stock')->default(true);
+            $table->boolean('on_sale')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
